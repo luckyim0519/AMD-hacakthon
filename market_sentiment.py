b97ad5327b2d61ca782f4fe70a74556f48a45703
luckyim <moon990519@gmail.com>
2024-02-25 22:21:30 -0500

    framework

---
+++ market_sentiment.py
@@ -0,0 +1,18 @@
+'''
+market sentiment prediction
+
+1. preprocessing the data
+    1. text cleaning
+    2. tokenization
+    3. stop word removal
+    4. lemmatizaiotn or steaming of data
+
+using Navie's Bayes algorithm with multiple features
+    #training the data
+    #testing the data
+
+'''
+
+
+
+
