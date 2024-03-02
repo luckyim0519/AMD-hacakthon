d1142c093d1a1db9e00a624e210e4c981653045e
shath1955 <74083424+shath1955@users.noreply.github.com>
2024-03-02 17:29:04 -0500

    added multinomial NB analyzer + associated data set

---
+++ market_sentiment_analyzer_Multinomial_NB.py
@@ -0,0 +1,57 @@
+import pandas as pd 
+import re
+
+from sklearn import preprocessing
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
+# Dummy data with labels 
+df = pd.read_csv('dataset_2_MNB.csv')
+
+df_review = df.iloc[:,0]
+df_label = df.iloc[:,2]
+
+# Data preprocessing 
+def preprocess_txt(text):
+   text = re.sub(r'[^a-zA-Z\s]', '', text)
+   text = text.lower()
+   return text
+
+df_review = df_review.apply(preprocess_txt)
+
+# Convert string label into nums
+le = preprocessing.LabelEncoder()
+df_label_enc = le.fit_transform(df_label)
+# Tokenize review 
+vec = CountVectorizer()
+df_review_tok = vec.fit_transform(df_review)
+
+# print("label: ", df_label_enc)
+# print("review: ", df_review_tok)
+
+#combine encoded features into one 
+#features = list(zip(df_review_tok, df_rating_enc))
+
+# Split data into training and testing sets (20% reserved for testing, 80% reserved for training)
+# Random_state seed with integer means shuffling is reproducable across mult func calls 
+X_train, X_test, y_train, y_test = train_test_split(df_review_tok, df_label_enc, test_size=0.2, random_state=42)
+
+#create multinomial Naive Bayes model 
+model = MultinomialNB()
+
+# Train the model on the training set
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+predictions = model.predict(X_test)
+
+# Evaluate the performance
+accuracy = accuracy_score(predictions, y_test)
+f1 = f1_score(predictions, y_test, average="weighted")
+print("test case: ", y_test)
+print("predictions: ", predictions)
+print("accuracy: ", accuracy)
+print("F1 score: ", f1)
