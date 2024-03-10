c4e2bffb439e71084abc1ef1c90bee7f5dbafb87
luckyim <moon990519@gmail.com>
2024-03-10 17:17:40 -0400

    integrated

--- market_analysis_website/src/App.js
+++ market_analysis_website/src/App.js
@@ -1,15 +1,31 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { OutputBar } from './components/OutputBar';
+import { Graph } from './components/Graph';
 
 function App() {
   return (
-    <div className="App">
-      <Banner />
-      <OutputBar />
 
-    </div>
+    <Router>
+      <div className="App">
+      
+        <Banner />
+        <Routes>
+          <Route path="/" element={<>
+            <OutputBar />
+          </>} />
+          <Route path="/graph" element={<Graph /> } />
+        </Routes>
+
+
+      </div>
+    </Router>
+    
+    
   );
 }
 
--- market_analysis_website/src/components/Graph.js
+++ market_analysis_website/src/components/Graph.js
@@ -1,22 +1,85 @@
-// Graph.js
+import React, { Component } from 'react';
+import { Chart } from 'react-google-charts';
 
-import React from 'react';
-import { PieChart, Pie } from 'recharts';
+export class Graph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comboChartData: null,
+      pieChartData: null
+    };
+  }
 
+  componentDidMount() {
+    this.drawComboChart();
+    this.drawPieChart();
+  }
 
-export const Graph = () => {
-    // Sample data
-    const data = [
-        { name: 'Geeksforgeeks', students: 400 },
-        { name: 'Technical scripter', students: 700 }
-     
+  drawComboChart() {
+    const comboData = [
+      ['Years', 'Positive', 'Neutral', 'Negative'],
+      ['2019', 50, 20, 30],
+      ['2020', 60, 30, 10],
+      ['2021', 50, 30, 20],
+      ['2022', 70, 20, 10],
+      ['2023', 60, 10, 30]
     ];
 
-    return (
-        <div className="graph-container"> {/* Apply container styles */}
-            <PieChart className="pie-chart" width={400} height={400}> {/* Apply chart styles */}
-                <Pie data={data} dataKey="students" outerRadius={150} fill="green" />
-            </PieChart>
-        </div>
+    const comboOptions = {
+      title: 'The Trend of Market Sentiment in Years',
+      vAxis: { title: 'Sentiment' },
+      hAxis: { title: 'Years' },
+      seriesType: 'bars',
+     
+    };
+
+    const comboChart = (
+      <Chart
+        width={'100vw'}
+        height={'500px'}
+        chartType="ComboChart"
+        loader={<div>Loading Chart</div>}
+        data={comboData}
+        options={comboOptions}
+      />
     );
+
+    this.setState({ comboChartData: comboChart });
+  }
+
+  drawPieChart() {
+    const pieData = [
+      ['Feedback', 'Numbers'],
+      ['Positive', 11],
+      ['Negative', 2],
+      ['Neutral', 2]
+    ];
+
+    const pieOptions = {
+      title: 'Concluded Feedback of Market',
+      pieHole: 0.4
+    };
+
+    const pieChart = (
+      <Chart
+        width={'100vw'}
+        height={'500px'}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={pieData}
+        options={pieOptions}
+      />
+    );
+
+    this.setState({ pieChartData: pieChart });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.comboChartData}</div>
+        <div>{this.state.pieChartData}</div>
+      </div>
+    );
+  }
 }
